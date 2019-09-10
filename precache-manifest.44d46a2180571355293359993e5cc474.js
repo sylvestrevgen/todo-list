5a35ba50a4a1f99279fec447ae0610235d4c847b
sylvestrevgen <sylvestrevgen@gmail.com>
2019-09-10 21:55:02 +0300

    Updates

---
+++ precache-manifest.44d46a2180571355293359993e5cc474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d19dd141b982ac489d081981cfa406",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e3fb0018e67258b0b3a6",
+    "url": "/todo-list/static/css/main.b4852bfb.chunk.css"
+  },
+  {
+    "revision": "bad425a601a4b7f23496",
+    "url": "/todo-list/static/js/2.f969c9df.chunk.js"
+  },
+  {
+    "revision": "e3fb0018e67258b0b3a6",
+    "url": "/todo-list/static/js/main.155ad2e5.chunk.js"
+  },
+  {
+    "revision": "51236a9f9133e160b8c3",
+    "url": "/todo-list/static/js/runtime~main.fb7e4658.js"
+  }
+]);
